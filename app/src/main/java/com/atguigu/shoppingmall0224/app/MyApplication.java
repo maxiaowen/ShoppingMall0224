0fd6c0bdcee7e7e67f780e070f07ea1001f64657
maxiaowen <332168460@qq.com>
2017-06-12 12:59:41 +0800

    * 请求主页数据和解析数据

---
+++ app/src/main/java/com/atguigu/shoppingmall0224/app/MyApplication.java
@@ -0,0 +1,33 @@
+package com.atguigu.shoppingmall0224.app;
+
+import android.app.Application;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initOkhttpUtils();
+
+    }
+
+    private void initOkhttpUtils() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .addInterceptor(new LoggerInterceptor("TAG"))
+                .connectTimeout(5000L, TimeUnit.MILLISECONDS)
+                .readTimeout(5000L, TimeUnit.MILLISECONDS)
+                //其他配置
+                .build();
+
+        OkHttpUtils.initClient(okHttpClient);
+    }
+}
