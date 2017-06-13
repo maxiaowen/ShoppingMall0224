44a00a61db83c114053ff2d0a858e40cdbc22cc3
maxiaowen <332168460@qq.com>
2017-06-13 16:33:39 +0800

    * 活动的点击事件

---
+++ app/src/main/java/com/atguigu/shoppingmall0224/home/bean/WebViewBean.java
@@ -0,0 +1,44 @@
+package com.atguigu.shoppingmall0224.home.bean;
+
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+
+import java.io.Serializable;
+
+public class WebViewBean implements Serializable {
+    /**
+     * icon_url : /operation/img/1478169868/1478761370286.png
+     * name : 尚硅谷福利专区之111.1专区
+     * url : /oper/1478169868app.html
+     */
+
+    private String icon_url;
+    private String name;
+    private String url;
+
+    public String getIcon_url() {
+        return icon_url;
+    }
+
+    public void setIcon_url(String icon_url) {
+        this.icon_url = icon_url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
+
