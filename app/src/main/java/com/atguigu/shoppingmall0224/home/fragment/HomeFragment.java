e1d6c0b8ce0f100455f78f65c8110c8b0eadae7e
maxiaowen <332168460@qq.com>
2017-06-14 15:08:04 +0800

    * 购物车展示页面

---
+++ app/src/main/java/com/atguigu/shoppingmall0224/home/fragment/HomeFragment.java
@@ -0,0 +1,138 @@
+package com.atguigu.shoppingmall0224.home.fragment;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.atguigu.shoppingmall0224.R;
+import com.atguigu.shoppingmall0224.base.BaseFragment;
+import com.atguigu.shoppingmall0224.home.adapter.HomeAdapter;
+import com.atguigu.shoppingmall0224.home.bean.HomeBean;
+import com.atguigu.shoppingmall0224.uilts.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import okhttp3.Call;
+
+import static com.atguigu.shoppingmall0224.R.id.ib_top;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+
+public class HomeFragment extends BaseFragment {
+    private static final String TAG = HomeFragment.class.getSimpleName();
+
+    Unbinder unbinder;
+    @BindView(R.id.tv_search_home)
+    TextView tvSearchHome;
+    @BindView(R.id.tv_message_home)
+    TextView tvMessageHome;
+    @BindView(R.id.rv_home)
+    RecyclerView rvHome;
+    @BindView(ib_top)
+    ImageButton ibTop;
+
+    private String homeUrl;
+    private HomeAdapter adapter;
+
+    @Override
+    public View initView() {
+        Log.e(TAG, "主页视图被初始化了");
+        View view = View.inflate(mContext, R.layout.fragment_home, null);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        Log.e(TAG, "绑定数据到控件上...");
+        getDataFromNet();
+
+    }
+    private void getDataFromNet() {
+        homeUrl = Constants.HOME_URL;
+        OkHttpUtils
+                .get()
+                .url(homeUrl)
+                .build()
+                .execute(new MyStringCallback());
+    }
+    class MyStringCallback extends StringCallback {
+
+        @Override
+        public void onError(Call call, Exception e, int id) {
+            Log.e(TAG,"请求失败=="+e.getMessage());
+        }
+
+        @Override
+        public void onResponse(String response, int id) {
+            Log.e(TAG,"请求成功==");
+            processData(response);
+
+        }
+    }
+
+    private void processData(String json) {
+        //解析数据
+        HomeBean homeBean = JSON.parseObject(json,HomeBean.class);
+        Log.e(TAG,"解析成功了=="+homeBean.getResult().getAct_info().get(0).getName());
+
+        //设置适配器
+        adapter = new HomeAdapter(mContext,homeBean.getResult());
+        rvHome.setAdapter(adapter);
+
+        //设置布局管理器
+        GridLayoutManager liner = new GridLayoutManager(mContext, 1);
+
+        liner.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (position <= 3) {
+                    ibTop.setVisibility(View.GONE);
+                } else {
+                    ibTop.setVisibility(View.VISIBLE);
+                }
+                return 1;
+            }
+        });
+
+
+        rvHome.setLayoutManager(liner);
+    }
+
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick({R.id.tv_search_home, R.id.tv_message_home, ib_top})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_search_home:
+                Toast.makeText(mContext, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_message_home:
+                Toast.makeText(mContext, "查看", Toast.LENGTH_SHORT).show();
+                break;
+            case ib_top:
+//                Toast.makeText(mContext, "回到顶部", Toast.LENGTH_SHORT).show();
+                rvHome.scrollToPosition(0);
+                break;
+        }
+    }
+}
--- app/src/main/java/com/atguigu/shoppingmall0224/shoppingcart/fragment/ShoppingcartFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall0224/shoppingcart/fragment/ShoppingcartFragment.java
@@ -1,11 +1,16 @@
 package com.atguigu.shoppingmall0224.shoppingcart.fragment;
 
-import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.atguigu.shoppingmall0224.R;
 import com.atguigu.shoppingmall0224.app.MyApplication;
 import com.atguigu.shoppingmall0224.base.BaseFragment;
 import com.atguigu.shoppingmall0224.home.bean.GoodsBean;
@@ -13,32 +18,91 @@ import com.atguigu.shoppingmall0224.shoppingcart.utils.CartStorage;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 /**
  * Created by Administrator on 2017/6/12.
  */
 
 public class ShoppingcartFragment extends BaseFragment {
     private static final String TAG = ShoppingcartFragment.class.getSimpleName();
-    private TextView textView;
+    @BindView(R.id.tv_shopcart_edit)
+    TextView tvShopcartEdit;
+    @BindView(R.id.recyclerview)
+    RecyclerView recyclerview;
+    @BindView(R.id.checkbox_all)
+    CheckBox checkboxAll;
+    @BindView(R.id.tv_shopcart_total)
+    TextView tvShopcartTotal;
+    @BindView(R.id.btn_check_out)
+    Button btnCheckOut;
+    @BindView(R.id.ll_check_all)
+    LinearLayout llCheckAll;
+    @BindView(R.id.cb_all)
+    CheckBox cbAll;
+    @BindView(R.id.btn_delete)
+    Button btnDelete;
+    @BindView(R.id.btn_collection)
+    Button btnCollection;
+    @BindView(R.id.ll_delete)
+    LinearLayout llDelete;
+    @BindView(R.id.iv_empty)
+    ImageView ivEmpty;
+    @BindView(R.id.tv_empty_cart_tobuy)
+    TextView tvEmptyCartTobuy;
+    @BindView(R.id.ll_empty_shopcart)
+    LinearLayout llEmptyShopcart;
+    Unbinder unbinder;
 
     @Override
     public View initView() {
-        Log.e(TAG,"购物车视图被初始化了");
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        textView.setTextColor(Color.RED);
-        return textView;
+        Log.e(TAG, "购物车视图被初始化了");
+        View view = View.inflate(mContext, R.layout.tfragment_shopping_car, null);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
+
     @Override
     public void initData() {
         super.initData();
-        Log.e(TAG,"购物车数据被初始化了");
-        textView.setText("购物车");
+        Log.e(TAG, "购物车数据被初始化了");
         ArrayList<GoodsBean> allData = CartStorage.getInstance(MyApplication.getContext()).getAllData();
-        for(int i = 0; i < allData.size(); i++) {
-            Log.e("TAG",""+allData.get(i).getName());
+    }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+
+    @OnClick({R.id.tv_shopcart_edit, R.id.checkbox_all, R.id.btn_check_out, R.id.cb_all, R.id.btn_delete, R.id.btn_collection, R.id.tv_empty_cart_tobuy})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_shopcart_edit:
+                Toast.makeText(mContext, "编辑", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.checkbox_all:
+                Toast.makeText(mContext, "全选", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_check_out:
+                Toast.makeText(mContext, "去结算", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cb_all:
+                Toast.makeText(mContext, "删除的全选", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_delete:
+                Toast.makeText(mContext, "删除", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_collection:
+                Toast.makeText(mContext, "收藏", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_empty_cart_tobuy:
+                Toast.makeText(mContext, "去逛逛", Toast.LENGTH_SHORT).show();
+                break;
         }
     }
 }
