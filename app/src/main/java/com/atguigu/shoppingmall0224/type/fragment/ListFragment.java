e21075a1596511c5b0d44650afe3605ddb54b616
maxiaowen <332168460@qq.com>
2017-06-16 19:10:14 +0800

    * 联网请求数据

--- app/src/main/java/com/atguigu/shoppingmall0224/type/fragment/ListFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall0224/type/fragment/ListFragment.java
@@ -1,17 +1,24 @@
 package com.atguigu.shoppingmall0224.type.fragment;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.shoppingmall0224.R;
 import com.atguigu.shoppingmall0224.base.BaseFragment;
 import com.atguigu.shoppingmall0224.type.adapter.TypeLeftAdapter;
+import com.atguigu.shoppingmall0224.type.bean.TypeBean;
+import com.atguigu.shoppingmall0224.uilts.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import okhttp3.Call;
 
 /**
  * 作者：杨光福 on 2017/6/16 15:40
@@ -21,6 +28,7 @@ import butterknife.Unbinder;
  */
 
 public class ListFragment extends BaseFragment {
+    private static final String TAG = ListFragment.class.getSimpleName();
     @BindView(R.id.lv_left)
     ListView lvLeft;
     @BindView(R.id.rv_right)
@@ -31,6 +39,10 @@ public class ListFragment extends BaseFragment {
 
     private String[] titles = new String[]{"小裙子", "上衣", "下装", "外套", "配件", "包包", "装扮", "居家宅品",
             "办公文具", "数码周边", "游戏专区"};
+
+    private String[] urls = new String[]{Constants.SKIRT_URL, Constants.JACKET_URL, Constants.PANTS_URL, Constants.OVERCOAT_URL,
+            Constants.ACCESSORY_URL, Constants.BAG_URL, Constants.DRESS_UP_URL, Constants.HOME_PRODUCTS_URL, Constants.STATIONERY_URL,
+            Constants.DIGIT_URL, Constants.GAME_URL};
     @Override
     public View initView() {
         View rootView = View.inflate(mContext, R.layout.fragment_list, null);
@@ -53,9 +65,42 @@ public class ListFragment extends BaseFragment {
                 typeLeftAdapter.changeSelectPosition(position);
                 //2.适配器刷新
                 typeLeftAdapter.notifyDataSetChanged();
+
+                //联网请求
+                getDataFromNet(urls[position]);
             }
         });
 
+        //联网请求
+        getDataFromNet(urls[0]);
+    }
+
+    private void getDataFromNet(String url) {
+        OkHttpUtils
+                .get()
+                .url(url)
+                .build()
+                .execute(new MyStringCallback());
+    }
+
+    class MyStringCallback extends StringCallback{
+        @Override
+        public void onError(Call call, Exception e, int id) {
+            Log.e(TAG, "请求成功失败==" + e.getMessage());
+        }
+
+        @Override
+        public void onResponse(String response, int id) {
+            Log.e(TAG, "请求成功=="+response);
+            processData(response);
+        }
+    }
+
+    private void processData(String json) {
+
+        TypeBean typeBean = JSON.parseObject(json,TypeBean.class);
+
+        Log.e("TAG","解析成功=="+ typeBean.getResult().get(0).getName());
 
     }
 
