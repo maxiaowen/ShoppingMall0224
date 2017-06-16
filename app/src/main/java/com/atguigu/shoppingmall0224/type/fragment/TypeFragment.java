69a761f3a1b85ba6d58d3b3acee652c23796eb62
maxiaowen <332168460@qq.com>
2017-06-16 16:04:16 +0800

    * 分类的布局实现和分析

--- app/src/main/java/com/atguigu/shoppingmall0224/type/fragment/TypeFragment.java
+++ app/src/main/java/com/atguigu/shoppingmall0224/type/fragment/TypeFragment.java
@@ -1,12 +1,19 @@
 package com.atguigu.shoppingmall0224.type.fragment;
 
-import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.atguigu.shoppingmall0224.R;
 import com.atguigu.shoppingmall0224.base.BaseFragment;
+import com.flyco.tablayout.SegmentTabLayout;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2017/6/12.
@@ -14,21 +21,54 @@ import com.atguigu.shoppingmall0224.base.BaseFragment;
 
 public class TypeFragment extends BaseFragment {
     private static final String TAG = TypeFragment.class.getSimpleName();
-    private TextView textView;
+    @BindView(R.id.tl_1)
+    SegmentTabLayout tl1;
+    @BindView(R.id.iv_type_search)
+    ImageView ivTypeSearch;
+    Unbinder unbinder;
+    Unbinder unbinder1;
+    private String[] titls = {"分类", "标签"};
 
     @Override
     public View initView() {
-        Log.e(TAG,"分类视图被初始化了");
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        textView.setTextColor(Color.RED);
-        return textView;
+        Log.e(TAG, "分类视图被初始化了");
+        View view = View.inflate(mContext, R.layout.fragment_type, null);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
+
     @Override
     public void initData() {
         super.initData();
-        Log.e(TAG,"分类数据被初始化了");
-        textView.setText("分类");
+        Log.e(TAG, "分类数据被初始化了");
+        tl1.setTabData(titls);
+        tl1.setOnTabSelectListener(new MyOnTabSelectListener());
+    }
+
+
+
+    class MyOnTabSelectListener implements OnTabSelectListener {
+        @Override
+        public void onTabSelect(int position) {
+            Toast.makeText(TypeFragment.this.getActivity(), "position==" + position, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onTabReselect(int position) {
+
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+
+    @OnClick(R.id.iv_type_search)
+    public void onViewClicked() {
+
+        Toast.makeText(TypeFragment.this.getContext(), "搜索", Toast.LENGTH_SHORT).show();
     }
 }
