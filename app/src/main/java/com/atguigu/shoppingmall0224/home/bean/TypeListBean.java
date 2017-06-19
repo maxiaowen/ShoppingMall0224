03cc35abd5007f52e71c4f3cd419d063af922131
maxiaowen <332168460@qq.com>
2017-06-19 11:52:15 +0800

    * 联网请求数据&解析数据

---
+++ app/src/main/java/com/atguigu/shoppingmall0224/home/bean/TypeListBean.java
@@ -0,0 +1,226 @@
+package com.atguigu.shoppingmall0224.home.bean;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/6/19.
+ */
+
+public class TypeListBean {
+
+    /**
+     * code : 200
+     * msg : 请求成功
+     * result : {"page_data":[{"product_id":"627","origin_price":"32.00","channel_id":"4","brand_id":"3","p_catalog_id":"52","supplier_type":"1","supplier_code":"1101001","name":"剑三T恤批发","cover_price":"32.00","brief":"","figure":"/1439367400560.jpg","sell_time_start":"1439308800","sell_time_end":"1439913600"},{"product_id":"3340","origin_price":"55.00","channel_id":"4","brand_id":"3","p_catalog_id":"28","supplier_type":"1","supplier_code":"1101036","name":"【小迷兔】剑网3剑三周边 泡面碗 天策 纯阳 明教","cover_price":"46.75","brief":"","figure":"/1478681924760.jpg","sell_time_start":"1478772000","sell_time_end":"1450108800"},{"product_id":"7591","origin_price":"69.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"1","supplier_code":"0","name":" 【同人寄卖】剑网3 剑三 炮太集合 同人Q版挂件 绘师孤九","cover_price":"10.00","brief":"现货","figure":"/1467018883461.jpg","sell_time_start":"1466956800","sell_time_end":"1467561600"},{"product_id":"3269","origin_price":"68.00","channel_id":"4","brand_id":"3","p_catalog_id":"90","supplier_type":"2","supplier_code":"802001","name":"[砚池工作室]剑三周边门派古风原创独家手工定制发光小夜灯","cover_price":"68.00","brief":"不包含提手~不包含提手~不包含提手~OVO 重要的si情嗦三遍~~~ 工期为5-10个工作日~\r\n","figure":"/1449219464141.jpg","sell_time_start":"1449158400","sell_time_end":"1449763200"},{"product_id":"3209","origin_price":"30.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"2","supplier_code":"802001","name":"[砚池工作室] 剑网3 剑三周边门派古风原创独家手工定制礼物木质卡套","cover_price":"26.40","brief":"","figure":"/1449134007474.jpg","sell_time_start":"1478772000","sell_time_end":"1449676800"},{"product_id":"2944","origin_price":"39.00","channel_id":"4","brand_id":"3","p_catalog_id":"77","supplier_type":"1","supplier_code":"1101036","name":"【小迷兔】剑网3 剑三周边 软面拉链零钱包 女式手拿包包 丐帮酒壶","cover_price":"33.15","brief":"","figure":"/1447999535316.jpg","sell_time_start":"1478772000","sell_time_end":"1448553600"},{"product_id":"15","origin_price":"90.00","channel_id":"4","brand_id":"3","p_catalog_id":"17","supplier_type":"1","supplier_code":"1101036","name":"【小迷兔】剑网3 剑三   长柄 自动 古风雨伞 夏日出行","cover_price":"76.50","brief":"剑网三 剑3 罗伞霖雨春 空谷幽兰 雨伞长柄 男女雨伞 出品：小迷兔的周边","figure":"/1460529658392.jpg","sell_time_start":"1478772000","sell_time_end":"0"},{"product_id":"3212","origin_price":"230.00","channel_id":"4","brand_id":"3","p_catalog_id":"90","supplier_type":"2","supplier_code":"802001","name":"[砚池工作室] 剑网3 剑三周边门派原创设计可调光充电台灯【乌夜啼】","cover_price":"202.40","brief":"此为下单定制。工期7-10天。由砚池发货。非质量问题下单不作退换。","figure":"/1474971558864.jpg","sell_time_start":"1478772000","sell_time_end":"1449676800"},{"product_id":"5","origin_price":"13.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"1","supplier_code":"0","name":"【衍生原创】剑侠情缘叁 剑网3 剑三护身符香包 挂件 御守","cover_price":"13.00","brief":"出品：喵叽叽叽大联萌","figure":"/1429840421737.jpg","sell_time_start":"0","sell_time_end":"0"},{"product_id":"2945","origin_price":"68.00","channel_id":"4","brand_id":"3","p_catalog_id":"105","supplier_type":"1","supplier_code":"1101036","name":"【小迷兔】剑网3周边门派LOGO 纯棉针织围巾 ","cover_price":"57.80","brief":"","figure":"/1447999737252.jpg","sell_time_start":"1478772000","sell_time_end":"1448553600"},{"product_id":"2514","origin_price":"15.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"1","supplier_code":"0","name":"【同人原创】剑网3 喵哥校服Q版 超萌喵叽 挂件","cover_price":"15.00","brief":"","figure":"/1446101440788.jpg","sell_time_start":"1446048000","sell_time_end":"1446652800"},{"product_id":"6414","origin_price":"20.00","channel_id":"4","brand_id":"3","p_catalog_id":"77","supplier_type":"1","supplier_code":"1101036","name":"【小迷兔】剑网3剑三周边 新宠卡通门派零钱包 小钱包 女士包","cover_price":"15.00","brief":"","figure":"/1469587052018.jpg","sell_time_start":"1462896000","sell_time_end":"1463500800"},{"product_id":"6113","origin_price":"45.00","channel_id":"4","brand_id":"182","p_catalog_id":"31","supplier_type":"1","supplier_code":"1101009","name":"【天闻角川】刀剑乱舞-ONLINE- 刀男折扇","cover_price":"45.00","brief":"","figure":"/1461665662734.jpg","sell_time_start":"1461600000","sell_time_end":"1462204800"},{"product_id":"3281","origin_price":"159.00","channel_id":"4","brand_id":"281","p_catalog_id":"71","supplier_type":"2","supplier_code":"100004","name":"【一方尘寰】剑三 万花 兰亭香雪 95大橙武发簪周边摆件预售","cover_price":"120.00","brief":"最后一次开95万花团了！！！最后500个完售后就正式封仓不再做了！！ \r\n大货样图已出！！改版之后更屌有木有！真金电镀绝对超值！ 实力打造，不容错过！ 用心打磨，只为愿您一个橙武梦！！！\r\n图为最终样，陆续会放大货高清大图！！实力打造，不容错过！ 用心打磨，只为圆尼一个橙武梦！！！","figure":"/1453194827823.jpg","sell_time_start":"0","sell_time_end":"0"},{"product_id":"3211","origin_price":"15.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"2","supplier_code":"802001","name":"[砚池工作室] 剑网3 剑三周边门派古风原创独家礼物 门派衍生金属书签","cover_price":"13.20","brief":"","figure":"/1449137495482.jpg","sell_time_start":"1478772000","sell_time_end":"1449676800"},{"product_id":"5774","origin_price":"92.00","channel_id":"4","brand_id":"3","p_catalog_id":"90","supplier_type":"2","supplier_code":"802001","name":"[砚池工作室]  剑网3 剑三周边门派古风原创独家定制礼物储蓄罐收纳盒","cover_price":"80.96","brief":"","figure":"/supplier/1478517808928.jpg","sell_time_start":"1478772000","sell_time_end":"1460044800"},{"product_id":"21","origin_price":"8.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"1","supplier_code":"0","name":"【同人原创】剑网3 剑侠情缘叁 Q版成男 口袋胸针","cover_price":"8.00","brief":"【同人原创】剑侠情缘叁 口袋胸针 剑网三系列","figure":"/1460456357286.jpg","sell_time_start":"0","sell_time_end":"0"},{"product_id":"1341","origin_price":"50.00","channel_id":"4","brand_id":"3","p_catalog_id":"24","supplier_type":"1","supplier_code":"0","name":"【蓝诺】《天下吾双》 剑网3同人本","cover_price":"50.00","brief":"","figure":"/1441850026602.jpg","sell_time_start":"1441814400","sell_time_end":"1442419200"},{"product_id":"1351","origin_price":"35.00","channel_id":"4","brand_id":"3","p_catalog_id":"104","supplier_type":"1","supplier_code":"0","name":"【钱塘夜画】剑网3 剑三 周边 11大门派手机包 手工小包","cover_price":"35.00","brief":"其余门派正在制作组 = 3=","figure":"/1441953751100.jpg","sell_time_start":"1441900800","sell_time_end":"1442505600"},{"product_id":"3540","origin_price":"79.00","channel_id":"4","brand_id":"3","p_catalog_id":"24","supplier_type":"1","supplier_code":"0","name":"【合志】【二刷现货】《苍天饶过谁》 剑网3 剑三  同人漫画","cover_price":"70.00","brief":"限量二刷现货~","figure":"/1450271803680.jpg","sell_time_start":"1450195200","sell_time_end":"1450800000"}],"catalog_data":false,"brand_data":false,"is_recommended":"0"}
+     */
+
+    private int code;
+    private String msg;
+    private ResultBean result;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public ResultBean getResult() {
+        return result;
+    }
+
+    public void setResult(ResultBean result) {
+        this.result = result;
+    }
+
+    public static class ResultBean {
+        /**
+         * page_data : [{"product_id":"627","origin_price":"32.00","channel_id":"4","brand_id":"3","p_catalog_id":"52","supplier_type":"1","supplier_code":"1101001","name":"剑三T恤批发","cover_price":"32.00","brief":"","figure":"/1439367400560.jpg","sell_time_start":"1439308800","sell_time_end":"1439913600"},{"product_id":"3340","origin_price":"55.00","channel_id":"4","brand_id":"3","p_catalog_id":"28","supplier_type":"1","supplier_code":"1101036","name":"【小迷兔】剑网3剑三周边 泡面碗 天策 纯阳 明教","cover_price":"46.75","brief":"","figure":"/1478681924760.jpg","sell_time_start":"1478772000","sell_time_end":"1450108800"},{"product_id":"7591","origin_price":"69.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"1","supplier_code":"0","name":" 【同人寄卖】剑网3 剑三 炮太集合 同人Q版挂件 绘师孤九","cover_price":"10.00","brief":"现货","figure":"/1467018883461.jpg","sell_time_start":"1466956800","sell_time_end":"1467561600"},{"product_id":"3269","origin_price":"68.00","channel_id":"4","brand_id":"3","p_catalog_id":"90","supplier_type":"2","supplier_code":"802001","name":"[砚池工作室]剑三周边门派古风原创独家手工定制发光小夜灯","cover_price":"68.00","brief":"不包含提手~不包含提手~不包含提手~OVO 重要的si情嗦三遍~~~ 工期为5-10个工作日~\r\n","figure":"/1449219464141.jpg","sell_time_start":"1449158400","sell_time_end":"1449763200"},{"product_id":"3209","origin_price":"30.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"2","supplier_code":"802001","name":"[砚池工作室] 剑网3 剑三周边门派古风原创独家手工定制礼物木质卡套","cover_price":"26.40","brief":"","figure":"/1449134007474.jpg","sell_time_start":"1478772000","sell_time_end":"1449676800"},{"product_id":"2944","origin_price":"39.00","channel_id":"4","brand_id":"3","p_catalog_id":"77","supplier_type":"1","supplier_code":"1101036","name":"【小迷兔】剑网3 剑三周边 软面拉链零钱包 女式手拿包包 丐帮酒壶","cover_price":"33.15","brief":"","figure":"/1447999535316.jpg","sell_time_start":"1478772000","sell_time_end":"1448553600"},{"product_id":"15","origin_price":"90.00","channel_id":"4","brand_id":"3","p_catalog_id":"17","supplier_type":"1","supplier_code":"1101036","name":"【小迷兔】剑网3 剑三   长柄 自动 古风雨伞 夏日出行","cover_price":"76.50","brief":"剑网三 剑3 罗伞霖雨春 空谷幽兰 雨伞长柄 男女雨伞 出品：小迷兔的周边","figure":"/1460529658392.jpg","sell_time_start":"1478772000","sell_time_end":"0"},{"product_id":"3212","origin_price":"230.00","channel_id":"4","brand_id":"3","p_catalog_id":"90","supplier_type":"2","supplier_code":"802001","name":"[砚池工作室] 剑网3 剑三周边门派原创设计可调光充电台灯【乌夜啼】","cover_price":"202.40","brief":"此为下单定制。工期7-10天。由砚池发货。非质量问题下单不作退换。","figure":"/1474971558864.jpg","sell_time_start":"1478772000","sell_time_end":"1449676800"},{"product_id":"5","origin_price":"13.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"1","supplier_code":"0","name":"【衍生原创】剑侠情缘叁 剑网3 剑三护身符香包 挂件 御守","cover_price":"13.00","brief":"出品：喵叽叽叽大联萌","figure":"/1429840421737.jpg","sell_time_start":"0","sell_time_end":"0"},{"product_id":"2945","origin_price":"68.00","channel_id":"4","brand_id":"3","p_catalog_id":"105","supplier_type":"1","supplier_code":"1101036","name":"【小迷兔】剑网3周边门派LOGO 纯棉针织围巾 ","cover_price":"57.80","brief":"","figure":"/1447999737252.jpg","sell_time_start":"1478772000","sell_time_end":"1448553600"},{"product_id":"2514","origin_price":"15.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"1","supplier_code":"0","name":"【同人原创】剑网3 喵哥校服Q版 超萌喵叽 挂件","cover_price":"15.00","brief":"","figure":"/1446101440788.jpg","sell_time_start":"1446048000","sell_time_end":"1446652800"},{"product_id":"6414","origin_price":"20.00","channel_id":"4","brand_id":"3","p_catalog_id":"77","supplier_type":"1","supplier_code":"1101036","name":"【小迷兔】剑网3剑三周边 新宠卡通门派零钱包 小钱包 女士包","cover_price":"15.00","brief":"","figure":"/1469587052018.jpg","sell_time_start":"1462896000","sell_time_end":"1463500800"},{"product_id":"6113","origin_price":"45.00","channel_id":"4","brand_id":"182","p_catalog_id":"31","supplier_type":"1","supplier_code":"1101009","name":"【天闻角川】刀剑乱舞-ONLINE- 刀男折扇","cover_price":"45.00","brief":"","figure":"/1461665662734.jpg","sell_time_start":"1461600000","sell_time_end":"1462204800"},{"product_id":"3281","origin_price":"159.00","channel_id":"4","brand_id":"281","p_catalog_id":"71","supplier_type":"2","supplier_code":"100004","name":"【一方尘寰】剑三 万花 兰亭香雪 95大橙武发簪周边摆件预售","cover_price":"120.00","brief":"最后一次开95万花团了！！！最后500个完售后就正式封仓不再做了！！ \r\n大货样图已出！！改版之后更屌有木有！真金电镀绝对超值！ 实力打造，不容错过！ 用心打磨，只为愿您一个橙武梦！！！\r\n图为最终样，陆续会放大货高清大图！！实力打造，不容错过！ 用心打磨，只为圆尼一个橙武梦！！！","figure":"/1453194827823.jpg","sell_time_start":"0","sell_time_end":"0"},{"product_id":"3211","origin_price":"15.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"2","supplier_code":"802001","name":"[砚池工作室] 剑网3 剑三周边门派古风原创独家礼物 门派衍生金属书签","cover_price":"13.20","brief":"","figure":"/1449137495482.jpg","sell_time_start":"1478772000","sell_time_end":"1449676800"},{"product_id":"5774","origin_price":"92.00","channel_id":"4","brand_id":"3","p_catalog_id":"90","supplier_type":"2","supplier_code":"802001","name":"[砚池工作室]  剑网3 剑三周边门派古风原创独家定制礼物储蓄罐收纳盒","cover_price":"80.96","brief":"","figure":"/supplier/1478517808928.jpg","sell_time_start":"1478772000","sell_time_end":"1460044800"},{"product_id":"21","origin_price":"8.00","channel_id":"4","brand_id":"3","p_catalog_id":"26","supplier_type":"1","supplier_code":"0","name":"【同人原创】剑网3 剑侠情缘叁 Q版成男 口袋胸针","cover_price":"8.00","brief":"【同人原创】剑侠情缘叁 口袋胸针 剑网三系列","figure":"/1460456357286.jpg","sell_time_start":"0","sell_time_end":"0"},{"product_id":"1341","origin_price":"50.00","channel_id":"4","brand_id":"3","p_catalog_id":"24","supplier_type":"1","supplier_code":"0","name":"【蓝诺】《天下吾双》 剑网3同人本","cover_price":"50.00","brief":"","figure":"/1441850026602.jpg","sell_time_start":"1441814400","sell_time_end":"1442419200"},{"product_id":"1351","origin_price":"35.00","channel_id":"4","brand_id":"3","p_catalog_id":"104","supplier_type":"1","supplier_code":"0","name":"【钱塘夜画】剑网3 剑三 周边 11大门派手机包 手工小包","cover_price":"35.00","brief":"其余门派正在制作组 = 3=","figure":"/1441953751100.jpg","sell_time_start":"1441900800","sell_time_end":"1442505600"},{"product_id":"3540","origin_price":"79.00","channel_id":"4","brand_id":"3","p_catalog_id":"24","supplier_type":"1","supplier_code":"0","name":"【合志】【二刷现货】《苍天饶过谁》 剑网3 剑三  同人漫画","cover_price":"70.00","brief":"限量二刷现货~","figure":"/1450271803680.jpg","sell_time_start":"1450195200","sell_time_end":"1450800000"}]
+         * catalog_data : false
+         * brand_data : false
+         * is_recommended : 0
+         */
+
+        private boolean catalog_data;
+        private boolean brand_data;
+        private String is_recommended;
+        private List<PageDataBean> page_data;
+
+        public boolean isCatalog_data() {
+            return catalog_data;
+        }
+
+        public void setCatalog_data(boolean catalog_data) {
+            this.catalog_data = catalog_data;
+        }
+
+        public boolean isBrand_data() {
+            return brand_data;
+        }
+
+        public void setBrand_data(boolean brand_data) {
+            this.brand_data = brand_data;
+        }
+
+        public String getIs_recommended() {
+            return is_recommended;
+        }
+
+        public void setIs_recommended(String is_recommended) {
+            this.is_recommended = is_recommended;
+        }
+
+        public List<PageDataBean> getPage_data() {
+            return page_data;
+        }
+
+        public void setPage_data(List<PageDataBean> page_data) {
+            this.page_data = page_data;
+        }
+
+        public static class PageDataBean {
+            /**
+             * product_id : 627
+             * origin_price : 32.00
+             * channel_id : 4
+             * brand_id : 3
+             * p_catalog_id : 52
+             * supplier_type : 1
+             * supplier_code : 1101001
+             * name : 剑三T恤批发
+             * cover_price : 32.00
+             * brief :
+             * figure : /1439367400560.jpg
+             * sell_time_start : 1439308800
+             * sell_time_end : 1439913600
+             */
+
+            private String product_id;
+            private String origin_price;
+            private String channel_id;
+            private String brand_id;
+            private String p_catalog_id;
+            private String supplier_type;
+            private String supplier_code;
+            private String name;
+            private String cover_price;
+            private String brief;
+            private String figure;
+            private String sell_time_start;
+            private String sell_time_end;
+
+            public String getProduct_id() {
+                return product_id;
+            }
+
+            public void setProduct_id(String product_id) {
+                this.product_id = product_id;
+            }
+
+            public String getOrigin_price() {
+                return origin_price;
+            }
+
+            public void setOrigin_price(String origin_price) {
+                this.origin_price = origin_price;
+            }
+
+            public String getChannel_id() {
+                return channel_id;
+            }
+
+            public void setChannel_id(String channel_id) {
+                this.channel_id = channel_id;
+            }
+
+            public String getBrand_id() {
+                return brand_id;
+            }
+
+            public void setBrand_id(String brand_id) {
+                this.brand_id = brand_id;
+            }
+
+            public String getP_catalog_id() {
+                return p_catalog_id;
+            }
+
+            public void setP_catalog_id(String p_catalog_id) {
+                this.p_catalog_id = p_catalog_id;
+            }
+
+            public String getSupplier_type() {
+                return supplier_type;
+            }
+
+            public void setSupplier_type(String supplier_type) {
+                this.supplier_type = supplier_type;
+            }
+
+            public String getSupplier_code() {
+                return supplier_code;
+            }
+
+            public void setSupplier_code(String supplier_code) {
+                this.supplier_code = supplier_code;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public String getCover_price() {
+                return cover_price;
+            }
+
+            public void setCover_price(String cover_price) {
+                this.cover_price = cover_price;
+            }
+
+            public String getBrief() {
+                return brief;
+            }
+
+            public void setBrief(String brief) {
+                this.brief = brief;
+            }
+
+            public String getFigure() {
+                return figure;
+            }
+
+            public void setFigure(String figure) {
+                this.figure = figure;
+            }
+
+            public String getSell_time_start() {
+                return sell_time_start;
+            }
+
+            public void setSell_time_start(String sell_time_start) {
+                this.sell_time_start = sell_time_start;
+            }
+
+            public String getSell_time_end() {
+                return sell_time_end;
+            }
+
+            public void setSell_time_end(String sell_time_end) {
+                this.sell_time_end = sell_time_end;
+            }
+        }
+    }
+}
