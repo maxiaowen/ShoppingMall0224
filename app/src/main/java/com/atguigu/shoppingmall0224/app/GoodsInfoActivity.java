5770a4589f39017bc0adeea2a1c3c1778315b300
maxiaowen <332168460@qq.com>
2017-06-19 16:41:42 +0800

    * 设置适配器

--- app/src/main/java/com/atguigu/shoppingmall0224/app/GoodsInfoActivity.java
+++ app/src/main/java/com/atguigu/shoppingmall0224/app/GoodsInfoActivity.java
@@ -100,7 +100,7 @@ public class GoodsInfoActivity extends AppCompatActivity {
         //设置数据
         tvGoodInfoName.setText(name);
         tvGoodInfoPrice.setText("￥"+cover_price);
-        Glide.with(this).load(Constants.BASE_URL_IMAGE + figure).into(ivGoodInfoImage);
+        Glide.with(this).load(Constants.BASE_URL_IMAGE + figure).placeholder(R.drawable.new_user_icon_background).error(R.drawable.new_user_icon_background).into(ivGoodInfoImage);
 
         //设置webView的数据
         setWebViewData(product_id);
